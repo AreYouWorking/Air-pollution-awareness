38728468723dbd7e54432fc60cc2ff637b39ed93
pooh <purichseenuallae@gmail.com>
2023-03-13 12:14:17 +0700

    fix color contrast

--- lib/style.dart
+++ lib/style.dart
@@ -4,10 +4,10 @@ import 'package:flutter/animation.dart';
 const greyUI = Color.fromRGBO(28, 28, 30, 1);
 
 const aqiColor = [
-  Color.fromRGBO(0, 153, 102, 1),
-  Color.fromRGBO(255, 222, 51, 1),
-  Color.fromRGBO(255, 153, 61, 1),
-  Color.fromRGBO(204, 0, 51, 1),
-  Color.fromRGBO(102, 0, 153, 1),
-  Color.fromRGBO(126, 0, 35, 1)
+  Color.fromRGBO(33, 233, 0, 1),
+  Color.fromRGBO(255, 214, 0, 1),
+  Color.fromRGBO(255, 107, 0, 1),
+  Color.fromRGBO(255, 82, 82, 1),
+  Color.fromRGBO(183, 123, 242, 1),
+  Color.fromRGBO(248, 37, 108, 1)
 ];
