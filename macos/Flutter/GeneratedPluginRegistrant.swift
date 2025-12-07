74cf0bd6a08002727824e414d0be8662c9c702a9
Pooh <purichseenuallae@gmail.com>
2025-12-07 20:35:34 +0700

    Update iOS and macOS project configurations and dependencies
    - Updated MinimumOSVersion in AppFrameworkInfo.plist from 11.0 to 13.0.
    - Included Podfile for CocoaPods dependency management.
    - Added debug and release xcconfig includes for Pods.
    - Created Podfile.lock with dependencies for iOS.
    - Modified Runner.xcodeproj to include new Pods configurations and updated object version.
    - Updated AppDelegate.swift to use @main instead of @UIApplicationMain.
    - Enhanced Info.plist with additional permissions for camera and location access.
    - Added generated_plugin_registrant.cc and generated_plugin_registrant.h for Linux plugins.
    - Updated pubspec.lock with new package versions and dependencies.
    - Created macOS Podfile and generated files for Flutter integration.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import file_selector_macos
+import geolocator_apple
+import path_provider_foundation
+import share_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+}
